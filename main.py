8ff2f5ba4912fee0f071d240ae9512f66d2c690f
edwin-ah <edwin.hallsten@hotmail.com>
2025-09-03 23:13:43 +0200

    first chapter

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("user_prompt")
+    parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
+    args = parser.parse_args()
+
+    if args.verbose:
+        print("verbosity turned on")
+    else:
+        print("verbosity turned off")
+
+    user_prompt = args.user_prompt
+    is_verbose = args.verbose
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages
+    )
+
+    print(response.text)
+    if is_verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
