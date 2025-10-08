0fdcec70327e277b7a883fcd056c7d0ffb64436b
edwin-ah <edwin.hallsten@hotmail.com>
2025-10-08 21:33:31 +0200

    Final change

--- main.py
+++ main.py
@@ -42,6 +42,10 @@ def main():
     - Write or overwrite files
 
     All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+    Assume relevant code is within the current project.
+    Start by exploring the file structure if it's asked about code you doesn't already know.
+    Use get_files_info to begin this exploration.
     """
 
     available_functions = types.Tool(
@@ -57,34 +61,63 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    response = client.models.generate_content(
-        model=model_name, 
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
-        )
-    )
+    should_continue = True
 
-    if response.function_calls:
-        for fc in response.function_calls:
-            tool_content = call_function(fc, verbose=is_verbose)
+    i = 1
+    while i <= 20 and should_continue:
+        did_call_tools = False
+        try:
+            response = client.models.generate_content(
+                model=model_name, 
+                contents=messages,
+                config=types.GenerateContentConfig(
+                    tools=[available_functions], system_instruction=system_prompt
+                )
+            )
+        except Exception as e:
+            print(f"Something went wrong trying to generate content: {e}")
+            break
 
-            # validate
-            parts = getattr(tool_content, "parts", []) # try to get the attribute parts from tool_content, [] if did not succeed
-            if not parts or not getattr(parts[0], "function_response", None):
-                raise RuntimeError("Function call returned invalid tool content")
-            resp = parts[0].function_response.response
-            if is_verbose:
-                print(f"-> {resp}")
-            
-    else:
-        if response.text:
-            print(response.text)
+        try:
+            for candidate in response.candidates:
+                if candidate.content:
+                    messages.append(candidate.content)
+                    for part in candidate.content.parts:
+                        if part.function_call:
+                            did_call_tools = True
+                            fc = part.function_call
+                            tool_content = call_function(fc, verbose=is_verbose)
+                            # validate
+                            parts = getattr(tool_content, "parts", []) # try to get the attribute parts from tool_content, [] if did not succeed
+                            if not parts or not getattr(parts[0], "function_response", None):
+                                raise RuntimeError("Function call returned invalid tool content")
+                            resp = parts[0].function_response.response
+                            func_resp = types.FunctionResponse(name=fc.name, response=resp)
+                            messages.append(types.Content(role="user", parts=[types.Part(function_response=func_resp)]))
 
-    if is_verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+                            if is_verbose:
+                                print(f"-> {resp}")
+            if did_call_tools:
+                continue
+            elif response.text:
+                print(response.text)
+                should_continue = False
+                break
+            else:
+                break
+
+        except RuntimeError as re:
+            print(f"There was a runtime error: {re}")
+            break
+        except Exception as e:
+            print(f"Something want wrong trying to call python file: {e}")
+            break
+                
+        if is_verbose:
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        i += 1
 
 if __name__ == "__main__":
     main()
